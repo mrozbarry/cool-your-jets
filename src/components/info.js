ab7814a0e92aa3c4bb3d46381fe4bace182b57db
Alex Barry <alex.barry@gmail.com>
2020-01-24 15:44:28 -0500

    Larger font for player info

--- src/components/info.js
+++ src/components/info.js
@@ -12,7 +12,7 @@ const text = ([x, y], text, lineWidth, style) => {
   return properties({
     lineWidth,
     ...styleProperty,
-    font: '16px screaming_neon',
+    font: '24px screaming_neon',
     textAlign: 'left',
     textBaseline: 'top',
   }, [
