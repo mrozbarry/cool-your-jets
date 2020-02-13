4a7e5c9ab01f1d7c1559c7597b5c4e135e0991ab
Alex Barry <alex.barry@gmail.com>
2020-02-12 19:52:38 -0500

    Some ui cleanup

--- src/ui/index.js
+++ src/ui/index.js
@@ -1,172 +1,7 @@
 import { app, h } from 'hyperapp';
 import * as subscriptions from '#/ui/subscriptions';
 import * as actions from '#/ui/actions';
-
-const overrideStyles = {
-  fontFamily: `'screaming_neon', sans-serif`,
-  fontSize: '36px',
-  backgroundColor: 'transparent',
-  color: 'white',
-  borderRadius: 0,
-};
-
-const formElementStyles = {
-  ...overrideStyles,
-  display: 'block',
-  border: 'none',
-  outline: 'none',
-  borderBottom: '3px white solid',
-  width: '100%',
-};
-
-const playerContainer = (props, children) => h(
-  'div',
-  {
-    ...(props || {}),
-    style: {
-      fontSize: '36px',
-      padding: '24px',
-      display: 'flex',
-      flexDirection: 'column',
-      alignItems: 'center',
-      justifyContent: 'space-around',
-      width: '100%',
-      height: '100%',
-    },
-  },
-  children,
-);
-
-const fieldset = (props, children) => h(
-  'fieldset',
-  {
-    ...(props || {}),
-    style: {
-      ...(props && props.style || {}),
-      width: '100%',
-      border: 'none',
-      outline: 'none',
-    },
-  },
-  children,
-);
-
-const label = ({ text, style }, children) => h(
-  'label',
-  {
-    style: {
-      display: 'block',
-      ...(style || {}),
-    },
-  },
-  [
-    text,
-    children,
-  ],
-);
-
-const row = (props, children) => h(
-  'div',
-  {
-    ...(props || {}),
-    style: {
-      display: 'flex',
-      alignItems: 'center',
-      justifyContent: 'center',
-      marginBottom: '32px',
-      width: '80%',
-      ...(props && props.style || {}),
-    },
-  },
-  children,
-);
-
-const playerSetup = ({
-  index,
-  controls,
-  name,
-  ready,
-  color: [_h, s, l],
-  controlOptions,
-  gameCountdown,
-}) => {
-  const [type, id] = controls.split('|');
-  const extraGamepadOption = type === 'gamepad' && id !== '-1'
-    ? [[controls, `Gamepad ${id}`]]
-    : [];
-
-  const options = controlOptions.concat(extraGamepadOption);
-
-  return h(playerContainer, null, [
-    row(null, [
-      fieldset(null, [
-        h(label, { text: 'Controls' }, [
-          h('select', {
-            style: {
-              ...formElementStyles,
-              width: '100%',
-            },
-            onchange: [actions.PlayerControls, (event) => ({ index, controls: event.target.value })],
-          }, options.map(([value, label]) => (
-            h('option', { value, selected: value === controls }, label)
-          ))),
-        ]),
-      ]),
-    ]),
-    row(null, [
-      h('div', {
-        style: {
-          border: '3px white solid',
-          backgroundColor: `hsl(${_h}, ${s}%, ${l}%)`,
-          width: '48px',
-          height: '48px',
-          cursor: 'pointer',
-          flex: '0 0 48px',
-          marginRight: '8px',
-
-        },
-        onclick: [actions.PlayerColor, { index }],
-      }),
-      fieldset({ style: { flex: 1 } }, [
-        h(label, { text: 'Name' }, [
-          h('input', {
-            value: name,
-            style: formElementStyles,
-            oninput: [actions.PlayerName, (event) => ({ index, name: event.target.value })],
-          }),
-        ]),
-      ]),
-    ]),
-    row(null, [
-      h('button', {
-        type: 'button',
-        style: {
-          ...overrideStyles,
-
-          borderWidth: '3px',
-          padding: '1rem',
-        },
-        disabled: (controls === '' || gameCountdown !== null),
-        onclick: [actions.PlayerReady, { index, ready: !ready }],
-      }, (gameCountdown === null ? (ready ? 'CANCEL' : 'READY') : `Starts in ${gameCountdown}`)),
-    ]),
-  ]);
-};
-
-const grid = ({ count }, children) => h(
-  'div',
-  {
-    style: {
-      display: 'grid',
-      gridTemplateColumns: '1fr 1fr',
-      gridTemplateRows: count > 2 ? '1fr 1fr' : '1fr',
-      gridGap: '3rem',
-      width: '100vw',
-      height: '100vh',
-    },
-  },
-  children,
-);
+import { grid, playerSetup, overrideStyles } from '#/ui/components';
 
 export default (node) => app({
   init: actions.Initialize,
@@ -232,11 +67,17 @@ export default (node) => app({
   subscriptions: state => {
     if (state.exit) return [];
 
+    const gamepadIndexes = state
+      .gamepadPlayers
+      .map(gpp => gpp.index)
+      .join(',');
+
     return [
       subscriptions.WaitForGamepad({
         clientId: state.clientId,
-        gamepadPlayers: state.gamepadPlayers,
+        gamepadIndexes,
         onGamepadDiscovered: actions.AddGamepad,
+        onAddingPlayer: actions.LockAddingPlayer,
       }),
     ];
   },
--- src/ui/subscriptions.js
+++ src/ui/subscriptions.js
@@ -2,25 +2,40 @@ import * as api from './api';
 
 const WaitForGamepadFX = (dispatch, {
   clientId,
-  gamepadPlayers,
+  gamepadIndexes,
   onAddingPlayer,
   onGamepadDiscovered,
 }) => {
+  let dead = false;
+  const id = Math.random().toString(36).slice(2);
+  console.log('Waiting for gamepads...', id);
   const now = performance.now();
   let handle = null;
 
-  const takenGamepadIndexes = gamepadPlayers.map(gp => gp.index);
+  const takenGamepadIndexes = gamepadIndexes
+    ? gamepadIndexes.split(',').map(Number)
+    : [];
+
+  console.log('Observing taken indexes', gamepadIndexes, takenGamepadIndexes);
 
   const checkButtons = async () => {
+    if (dead) return;
     const gamepads = navigator.getGamepads();
     let gamepad;
     for(gamepad of gamepads) {
       if (!gamepad) continue;
-      if (takenGamepadIndexes.includes(gamepad.index)) continue;
-      if (gamepad.timestamp < now) continue; 
+      if (takenGamepadIndexes.includes(gamepad.index)) {
+        console.log('checkButtons', id, 'skipping', gamepad.index, 'since it is already taken');
+        continue;
+      }
+      if (gamepad.timestamp < now) {
+        console.log('checkButtons', id, 'skipping', gamepad.index, 'since it has not been updated in a while');
+        continue; 
+      }
 
       dispatch(onAddingPlayer);
       const player = await api.addPlayer(clientId);
+      console.log('gamepad discovered', gamepad);
       dispatch(onGamepadDiscovered, {
         index: gamepad.index,
         identifier: player.identifier,
@@ -33,6 +48,8 @@ const WaitForGamepadFX = (dispatch, {
   checkButtons();
 
   return () => {
+    dead = true;
+    console.log('Stopped waiting for gamepads', id);
     cancelAnimationFrame(handle);
   };
 };
